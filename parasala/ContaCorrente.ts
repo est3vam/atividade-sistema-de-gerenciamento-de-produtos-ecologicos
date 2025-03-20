38b4cb3f0e521a0f948096d6a91bfccdc06f7c58
unknown <estevinn66@gmail.com>
2025-03-20 13:03:37 -0300

    pomba

---
+++ parasala/ContaCorrente.ts
@@ -0,0 +1,25 @@
+import { contaBancaria } from "./contabancaria";
+class ContaCorrente implements contaBancaria{
+    saldo:number
+    constructor(saldoInicial:number){
+        this.saldo = saldoInicial;
+    }
+
+    depositar(valor:number){
+        this.saldo += valor;
+
+    }
+
+    sacar(valor:number):void{
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+        }
+        else{console.log("value baixo seu podreca");
+
+        }
+    }
+}
+const conta = new ContaCorrente(1000);
+
+conta.depositar(200);
+conta.sacar(9000000000000);
