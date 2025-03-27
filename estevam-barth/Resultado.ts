67b6e38c72d30ce674a753e126cbc67d83a6c667
unknown <estevinn66@gmail.com>
2025-03-27 13:25:05 -0300

    pronto com teste sem receber parametro

--- estevam-barth/Resultado.ts
+++ estevam-barth/Resultado.ts
@@ -1,11 +1,20 @@
 import { AlimentoOrganico } from "./AlimentoOrganico";
+import { GerenciadorProdutos } from "./GerenciadorProdutos";
 import { ProdutoLimpezaBiodegradavel } from "./ProdutoLimpezaBiodregadavel";
 import { itemDecoracaoSustentavel } from "./itemDecoracaoSustentavel";
+
+const gerenciador = new GerenciadorProdutos();
 
 const maca = new AlimentoOrganico("maça organica",5.99, new Date("2024-12-20"));
 const detergente = new ProdutoLimpezaBiodegradavel("Detergente Natural", 12.50,500);
 const vaso = new itemDecoracaoSustentavel("vaso de vidro", 29.90, "vidro reciclado");
 
-console.log(maca);
-console.log(detergente);
-console.log(vaso);
+maca.exibirDetalhes();
+detergente.exibirDetalhes();
+vaso.exibirDetalhes();
+
+gerenciador.adicionarProduto(maca);
+gerenciador.adicionarProduto(detergente);
+gerenciador.adicionarProduto(vaso);
+
+gerenciador.listarProdutos();
